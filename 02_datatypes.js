b2c8b7a3efd98880b4f6e3d578bf3b2fd05b3338
gitdemo <135804234+ExplorerSoul@users.noreply.github.com>
2024-02-01 21:27:01 +0530

    data types basics

---
+++ 02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3)  // we are using nodejs, not browser
+
+console.log(3
+    +
+    3)  // code readability should be high
+
+console.log("amit")
+let age = 21
+let isLoggedIn = false
+let state; // gives undefined
+let vill = null // gives you null means nothing // but it is defined
+
+// number => 2 to the power 53
+// bigint- for largerr integer values eg. stocks
+// string => ""
+// boolean => true/false
+// null => standalone
+// undefined => declaared but no values assigned
+// symbol => unique
+/*
+use this link to learn more about js datatypes
+"https://tc39.es/ecma262/#sec-ecmascript-data-types-and-values"
+"https://developer.mozilla.org/en-US/docs/Web/javascript"
+*/
+
+// object
+console.log(typeof nulll); // object
+console.log(typeof undefined); // undefined
