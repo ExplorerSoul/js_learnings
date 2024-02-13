e70f0c790e9b3f522e84ea89237c58bfc82d33cf
gitdemo <135804234+ExplorerSoul@users.noreply.github.com>
2024-02-14 00:03:27 +0530

    Comparision of datatypes

---
+++ 04_comparision.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2>+1);
+// console.log(3<7);
+// console.log(2==1);
+// console.log(2>=3);
+
+
+// console.log(undefined<0);
+// console.log(undefined>0);
+// console.log(undefined==0);
+// console.log(undefined>=0);
+
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null<0);
+console.log(null>=0);   //true
+
+// ===
+console.log("2" === 2); // === checks comparision wrt datatype
+
+
