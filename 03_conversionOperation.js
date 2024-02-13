119330299b5bfa026ba2a4d62c6390b0145eb669
gitdemo <135804234+ExplorerSoul@users.noreply.github.com>
2024-02-13 23:09:16 +0530

    Conversion of Datatypes

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -22,6 +22,50 @@ let valueInNumber = Number(score);
 
  let someNumber = 33
  let stringNumber  = String(someNumber)
- console.log(stringNumber)
- console.log(typeof stringNumber)
+//  console.log(stringNumber)
+//  console.log(typeof stringNumber)
+
+// ****************** OPERATIONS *******************
+
+let val = 3
+let negval = -val
+// console.log(negval);
+
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2/3);
+// console.log(2**3);  // to the power
+// console.log(2%3);   // remainder
+
+
+let str1 = "hello"
+let str2 = " Amit"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(3 + 4 * 5 % 2); // never write these kind of massy codes
+// console.log(true);
+// console.log(+true); 
+// console.log(true+); // gives error
+// console.log(+"");
+
+// let num1, num2, num3
+// num1 = num2 =num3 = 3+32
+
+// let gameCounter = 100
+// gameCounter++
+// console.log(gameCounter); 
+
+// link to study
+//https://tc39.es/ecma262/#sec-abstract-operations
+
+// study about prefix and postfix
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
 
