ba27772892e91e07090fb0bec9b430791aa82570
gitdemo <135804234+ExplorerSoul@users.noreply.github.com>
2024-02-20 01:55:17 +0530

    Create 07_string.js

---
+++ 07_string.js
@@ -0,0 +1,12 @@
+const name = "Amit"
+const repoCount = 6
+
+// console.log(name + repoCount + "Value");     // old school method
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);      // modern method
+
+const gameName = new String('amitcc')
+
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
